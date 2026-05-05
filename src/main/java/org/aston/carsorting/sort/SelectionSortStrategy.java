d61c72099a353e10ea7ec4b2b1338077c777fc32
Sargalis <lislenov@yandex.ru>
2026-05-05 21:42:33 +0300

    feat: SelectionSortStrategy
    Implemented SelectionSortStrategy algorithm

--- src/main/java/org/aston/carsorting/sort/SelectionSortStrategy.java
+++ src/main/java/org/aston/carsorting/sort/SelectionSortStrategy.java
@@ -3,11 +3,22 @@ package org.aston.carsorting.sort;
 import org.aston.carsorting.model.Car;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class SelectionSortStrategy implements SortStrategy {
 
 	@Override
 	public void sort(ArrayList<Car> cars) {
-
+		for (int i = 0; i < cars.size() - 1; i++) {
+			int minIndex = i;
+			for (int j = i + 1; j < cars.size(); j++) {
+				if (Comparator.compare(cars.get(minIndex), cars.get(j)) > 0) {
+					minIndex = j;
+				}
+			}
+			if (i != minIndex) {
+				Collections.swap(cars, i, minIndex);
+			}
+		}
 	}
 }
