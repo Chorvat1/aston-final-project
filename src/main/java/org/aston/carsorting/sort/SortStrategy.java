1b0a84d8760788969772bf404752a5e354abb438
Sargalis <lislenov@yandex.ru>
2026-05-05 21:09:06 +0300

    feat: SortStrategy
    Added SortStrategy interface

--- src/main/java/org/aston/carsorting/sort/SortStrategy.java
+++ src/main/java/org/aston/carsorting/sort/SortStrategy.java
@@ -1,4 +1,9 @@
 package org.aston.carsorting.sort;
 
+import org.aston.carsorting.model.Car;
+
+import java.util.ArrayList;
+
 public interface SortStrategy {
+	public void sort(ArrayList<Car> cars);
 }
