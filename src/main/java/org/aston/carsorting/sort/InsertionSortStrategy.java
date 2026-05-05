8d684ba0d687f86b032f2006472c483be98a0ad0
Sargalis <lislenov@yandex.ru>
2026-05-05 21:42:14 +0300

    feat: InsertionSortStrategy
    Implemented InsertionSortStrategy algorithm

--- src/main/java/org/aston/carsorting/sort/InsertionSortStrategy.java
+++ src/main/java/org/aston/carsorting/sort/InsertionSortStrategy.java
@@ -8,6 +8,14 @@ public class InsertionSortStrategy implements SortStrategy {
 
 	@Override
 	public void sort(ArrayList<Car> cars) {
-
+		for (int i = 1; i < cars.size() - 1; i++) {
+			Car temp = cars.get(i);
+			int j = i;
+			while (j > 0 && Comparator.compare(cars.get(j-1),temp) > 0){
+				cars.set(j,cars.get(j-1));
+				j--;
+			}
+			cars.set(j,temp);
+		}
 	}
 }
