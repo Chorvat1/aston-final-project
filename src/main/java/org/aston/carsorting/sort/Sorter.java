bc2fbcbe671cf7c5e36e56624dfcd19757cd6b1e
Sargalis <lislenov@yandex.ru>
2026-05-05 21:09:32 +0300

    feat: Sorter
    Added Sorter class

--- src/main/java/org/aston/carsorting/sort/Sorter.java
+++ src/main/java/org/aston/carsorting/sort/Sorter.java
@@ -1,4 +1,17 @@
 package org.aston.carsorting.sort;
 
+import org.aston.carsorting.model.Car;
+
+import java.util.ArrayList;
+
 public class Sorter {
+	private SortStrategy sortStrategy;
+
+	public Sorter(SortStrategy strategy) {
+		this.sortStrategy = strategy;
+	}
+
+	public void sort(ArrayList<Car> cars) {
+		sortStrategy.sort(cars);
+	}
 }
