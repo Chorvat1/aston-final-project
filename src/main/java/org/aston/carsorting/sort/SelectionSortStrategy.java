09ad923db568551c5db93748589a9ccb00c5fb7d
Sargalis <lislenov@yandex.ru>
2026-05-11 19:59:30 +0300

    feat: TotalComparator for sorting

--- src/main/java/org/aston/carsorting/sort/SelectionSortStrategy.java
+++ src/main/java/org/aston/carsorting/sort/SelectionSortStrategy.java
@@ -1,5 +1,6 @@
 package org.aston.carsorting.sort;
 
+import org.aston.carsorting.comparator.TotalComparator;
 import org.aston.carsorting.model.Car;
 
 import java.util.ArrayList;
@@ -9,6 +10,7 @@ public class SelectionSortStrategy implements SortStrategy {
 
 	@Override
 	public void sort(ArrayList<Car> cars) {
+		TotalComparator Comparator = new TotalComparator();
 		for (int i = 0; i < cars.size() - 1; i++) {
 			int minIndex = i;
 			for (int j = i + 1; j < cars.size(); j++) {
