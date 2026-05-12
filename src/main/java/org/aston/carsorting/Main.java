513d18d5bdf97d299741a916305c08a880bb53ae
Sargalis <lislenov@yandex.ru>
2026-05-13 01:29:14 +0300

    ref: public dop variable / local call for dop1

--- src/main/java/org/aston/carsorting/Main.java
+++ src/main/java/org/aston/carsorting/Main.java
@@ -17,11 +17,11 @@ public class Main {
     private static final Scanner scanner = new Scanner(System.in);
     private static final String DEFAULT_FILE_PATH = "src/main/resources/cars.txt";
     private static CarList currentCars;
-    //Параметры доп заданий
-    static boolean dop1 = false;
-    static boolean dop2 = false;
-    //static boolean dop3 = false;
-    //static boolean dop4 = false;
+    //Параметры доп заданий (предлагаю оставить публичными, чтобы по месту реализации чек делать)
+    public static boolean dop1 = false;
+    public static boolean dop2 = false;
+    //public static boolean dop3 = false;
+    //public static boolean dop4 = false;
 
     public static void main(String[] args) {
 
@@ -131,7 +131,7 @@ public class Main {
 
         System.out.println("Сортируем...");
         //Use:
-        sorter.sort(carList, dop1); //true если нужна сортировка по доп_заданию 1
+        sorter.sort(carList);
         //sorter.sort(carList, new TotalComparator());
 
         System.out.println("--- ОТСОРТИРОВАННЫЙ СПИСОК АВТОМОБИЛЕЙ ---");
